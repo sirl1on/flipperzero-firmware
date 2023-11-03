53d7d32db32dd8e0d9ed52def42819edf99c1d3b
sirl1on <github@sirl1on.de>
2023-11-03 14:54:36 +0100

    WIP

--- applications/main/nfc/plugins/supported_cards/mensacard.c
+++ applications/main/nfc/plugins/supported_cards/mensacard.c
@@ -21,10 +21,8 @@
 #include "nfc_supported_card_plugin.h"
 
 #include <flipper_application/flipper_application.h>
-#include <applications/services/locale/locale.h>
-#include <furi_hal_rtc.h>
-
 #include <lib/nfc/protocols/mf_desfire/mf_desfire.h>
+#include <lib/nfc/protocols/iso14443_4a/iso14443_4a_i.h>
 
 static const MfDesfireApplicationId mensacard_app_id = {.data = {0x5f, 0x84, 0x15}};
 
@@ -38,33 +36,48 @@ static double rawDataToDouble(const char* data) {
     return (double)value / 1000;
 }
 
+static uint64_t parseUid(uint8_t raw_uid[], uint8_t uid_len) {
+    assert(uid_len <= 8);
+    uint64_t uid = 0;
+    for(size_t i = 0; i < uid_len; i++) {
+        uid |= (uint64_t)raw_uid[i] << (i * 8);
+    }
+    return uid;
+}
+
 static bool mensacard_parse(const NfcDevice* device, FuriString* parsed_data) {
     furi_assert(device);
     furi_assert(parsed_data);
 
-    const MfDesfireData* data = nfc_device_get_data(device, NfcProtocolMfDesfire);
-    bool parsed = false;
+    const MfDesfireData* device_data = nfc_device_get_data(device, NfcProtocolMfDesfire);
+    const MfDesfireApplication* app = mf_desfire_get_application(device_data, &mensacard_app_id);
+    if(app == NULL) {
+        return false;
+    }
 
-    do {
-        const MfDesfireApplication* app = mf_desfire_get_application(data, &mensacard_app_id);
-        if(app == NULL) break;
+    const MfDesfireFileSettings* file_settings =
+        mf_desfire_get_file_settings(app, &mensacard_file_id);
 
-        const MfDesfireFileSettings* file_settings =
-            mf_desfire_get_file_settings(app, &mensacard_file_id);
-        if(file_settings == NULL || file_settings->type != MfDesfireFileTypeValue) {
-            break;
-        }
+    if(file_settings == NULL || file_settings->type != MfDesfireFileTypeValue) {
+        return false;
+    }
 
-        const MfDesfireFileData* file_data = mf_desfire_get_file_data(app, &mensacard_file_id);
-        if(file_data == NULL) break;
+    const MfDesfireFileData* file_data = mf_desfire_get_file_data(app, &mensacard_file_id);
+    if(file_data == NULL) {
+        return false;
+    }
 
-        const char* data = simple_array_cget_data(file_data->data);
+    furi_string_cat_printf(
+        parsed_data,
+        "Mensa Card\n%llu\n\n",
+        parseUid(
+            device_data->iso14443_4a_data->iso14443_3a_data->uid,
+            device_data->iso14443_4a_data->iso14443_3a_data->uid_len));
 
-        furi_string_printf(parsed_data, "Balance: %.2f EUR", rawDataToDouble(data));
-        parsed = true;
-    } while(false);
+    const char* data = simple_array_cget_data(file_data->data);
 
-    return parsed;
+    furi_string_cat_printf(parsed_data, "Balance: %.2f EUR\n", rawDataToDouble(data));
+    return true;
 }
 
 /* Actual implementation of app<>plugin interface */
